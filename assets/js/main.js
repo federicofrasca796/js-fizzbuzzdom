2dcf513e4d0e71d535f17ae65d7518b59d2a0647
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-13 17:48:51 +0200

    complete FOR cycle&if condition to check divisibility

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,33 +4,48 @@ per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di
 */
 
 //inserire titolo
-const header = document.querySelector("header")
-const h1 = document.createElement("h1")
-h1.append('FizzBuzzDOM')
-header.append(h1)
+const header = document.querySelector("header");
+const h1 = document.createElement("h1");
+h1.append('FizzBuzzDOM');
+header.append(h1);
 
 
 //inserire un elemento <ul> nel main
-const main = document.querySelector("main")
-const ulElement = document.createElement("ul")
-
-main.append(ulElement)
+const main = document.querySelector("main");
+const ulElement = document.createElement("ul");
+main.append(ulElement);
 
 //Esegui un ciclo da 1 a 100 e stampa i numeri dentro dei list-item contenuti dentro ulElement
-for (let i = 0; i <= 100; i++){
-    if (i % 3 == 0){
-        //se è divisibile per 3
-        ulElement.innerHTML += `<li class="square"> fizz </li>`
-    } else if (i % 5 == 0){
-        //se è divisibile per 5
-        ulElement.innerHTML += `<li class="square"> buzz </li>`
-    } else if (i % 3 == 0 && i % 5 == 0){
-        //se è divisibile per 5 e per 3
-        ulElement.innerHTML += `<li class="square"> fizzbuzz </li>`
-    } else {
-        //se non è divisibile per nessuno dei due
-        ulElement.innerHTML += `<li class="square"> ${i} </li>`
+for (let i = 1; i <= 100; i++){
+    //creo elemento list item dentro ulElement
+    const liElement = document.createElement("li");
+    ulElement.append(liElement);
+    liElement.className = "square";
+
+    //verifico divisibilità del numero e attribuisco rispettive classi
+    if (i % 3 == 0 && i % 5 == 0){
+
+        //sovrascrivo il contenuto del list item
+        liElement.innerHTML = "fizzbuzz";
+        //aggiungo rispettiva classe
+        liElement.className += " " + "fizzbuzz";
+
+    } else if (i % 3 == 0) {
+
+        //sovrascrivo il contenuto del list item
+        liElement.innerHTML = "fizz";
+        //aggiungo rispettiva classe
+        liElement.className += " " + "fizz";
+
+    } else if (i % 5 == 0) {
+
+        //sovrascrivo il contenuto del list item
+        liElement.innerHTML = "buzz";
+        //aggiungo rispettiva classe
+        liElement.className += " " + "buzz";
+
+    } else{
+
+        liElement.innerHTML = i;
     }
 }
-
-// if (i)
